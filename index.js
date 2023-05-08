3423eae73d28b689bc12c6d0a6ddcf9c3eac6663
motsu8e <motsu159357@gmail.com>
2023-05-08 17:55:27 +0900

    burgerカウント実装

--- index.js
+++ index.js
@@ -1,95 +1,22 @@
-const config = {
-    welcomePage: document.getElementById("welcome"),
-    gamePage: document.getElementById("game"),
-    name: document.getElementById("userName"),
-};
-
-const commodity = [
-    {
-        name: "buger",
-        isSecInc: false,
-        return: 25,
-        maxPurchases: 500,
-        price: 15000,
-    },
-    {
-        name: "ETF Stock",
-        isSecInc: true,
-        return: 0.001,
-        maxPurchases: Infinity,
-        price: 300000,
-    },
-    {
-        name: "ETF Bounds",
-        isSecInc: true,
-        return: 0.0007,
-        maxPurchases: Infinity,
-        price: 300000,
-    },
-    {
-        name: "Lemonade Stand",
-        isSecInc: true,
-        return: 30,
-        maxPurchases: 1000,
-        price: 30000,
-    },
-    {
-        name: "Ice Cream Truck",
-        isSecInc: true,
-        return: 120,
-        maxPurchases: 500,
-        price: 100000,
-    },
-    {
-        name: "House",
-        isSecInc: true,
-        return: 32000,
-        maxPurchases: 100,
-        price: 20000000,
-    },
-    {
-        name: "TownHouse",
-        isSecInc: true,
-        return: 64000,
-        maxPurchases: 100,
-        price: 40000000,
-    },
-    {
-        name: "Mansion",
-        isSecInc: true,
-        return: 500000,
-        maxPurchases: 20,
-        price: 250000000,
-    },
-    {
-        name: "Industrial Space",
-        isSecInc: true,
-        return: 2200000,
-        maxPurchases: 10,
-        price: 1000000000,
-    },
-    {
-        name: "Hotel Skyscraper",
-        isSecInc: true,
-        return: 25000000,
-        maxPurchases: 5,
-        price: 10000000000,
-    },
-    {
-        name: "Bullet-Speed Sky Railway",
-        isSecInc: true,
-        return: 30000000000,
-        maxPurchases: 1,
-        price: 10000000000000,
-    },
-];
+class Commodity {
+    constructor(name, isSecInc, returnMoney, maxAmount, price) {
+        this.name = name;
+        this.isSecInc = isSecInc;
+        this.returnMoney = returnMoney;
+        this.maxAmount = maxAmount;
+        this.currentAmount = 0;
+        this.price = price
+    }
+}
 
 class User {
     constructor(name) {
         this.name = name;
-        this.old = 20;
+        this.age = 20;
         this.days = 0;
         this.money = 15000;
+        // key: commodityName
+        // value: amount
         this.hasCommodity = new Map();
         for (let ele of commodity) {
             this.hasCommodity.set(ele.name, 0);
@@ -97,22 +24,26 @@ class User {
     }
 }
 
-class Commodity {
-    returnMoney = 0;
-    constructor() {
-        this.amount = 0;
-        this.totalReturn = 0;
-    }
-    setAmount() {
-        this.amount += 1;
-    }
-    setIncreaseMoney() {
-        this.totalReturn = this.returnMoney * this.amount;
-    }
-    getMoney() {
-        return this.totalReturn;
-    }
-}
+const config = {
+    welcomePage: document.getElementById("welcome"),
+    gamePage: document.getElementById("game"),
+    name: document.getElementById("userName"),
+};
+
+const commodity = [
+    new Commodity("burger", false, 25, Infinity, 0,),
+    new Commodity("Flip", true, 25, Infinity, 500,),
+    new Commodity("ETF Stock", true, 0.001, Infinity, 300000,),
+    new Commodity("ETF Bounds", true, 0.0007, Infinity, 300000,),
+    new Commodity("Lemonade Stand", true, 30, 1000, 30000,),
+    new Commodity( "Ice Cream Truck", true, 120, 100000,),
+    new Commodity("House", true, 32000, 100, 20000000,),
+    new Commodity("TownHouse", true, 64000, 100, 40000000,),
+    new Commodity("Mansion", true, 500000, 20, 250000000,),
+    new Commodity("Industrial Space", true, 2200000, 10, 1000000000,),
+    new Commodity("Hotel Skyscraper", true, 25000000, 5, 10000000000,),
+    new Commodity("Bullet-Speed Sky Railway", true, 30000000000, 1, 10000000000000,),
+];
 
 function hidePage(ele) {
     ele.classList.remove("d-block");
@@ -132,34 +63,74 @@ function initializeUser() {
 }
 
 function createGamePage(user) {
-    const gamePage = document.createElement("div");
-    gamePage.classList.add(
+    const container = document.createElement("div");
+    container.classList.add(
         "container",
+        "text-white",
         "align-items-center",
         "vh-100",
         "d-flex",
         "justify-content-center"
     );
-    gamePage.innerHTML = `
-        <div id="buger" class="col-4">Buger</div>
-        <div class="col-7">
-            ${createUserInfo(user)}
-            <div id="buyArea">123456</div>
+    // container.innerHTML = `
+    // ${createBurger(user)}
+    // ${createUserInfo(user)}
+    // `;
+
+    container.append(createBurger(user))
+    container.append(createUserInfo(user))
+  
+    return container;
+}
+
+function updateGamePage(user){
+    config.gamePage.innerHTML = "";
+    config.gamePage.append(createGamePage(user));
+}
+
+function createBurger(user){
+    const burger = document.createElement("div");
+    burger.setAttribute("id", "burger");
+    burger.classList.add("col-4")
+    burger.innerHTML = `
+        <div id="burgerInfo" class="d-flex flex-column align-items-center bg-navy my-3">
+            <div>${user.hasCommodity.get("burger")} Burger</div>
+            <div>1click ${user.hasCommodity.get("Flip")}</div>
+        </div>
+        <div>
+            <div>
+                <img src="https://cdn.pixabay.com/photo/2014/04/02/17/00/burger-307648_960_720.png" width=80%  id="burgerImg">
+            </div>
         </div>
     `;
-    return gamePage;
+
+    const burgerEvent = burger.querySelectorAll("#burgerImg")[0]
+    burgerEvent.addEventListener("click", ()=>{
+        const current = user.hasCommodity.get("burger")
+        user.hasCommodity.set("burger", current+1)
+        updateGamePage(user);
+        console.log(user.hasCommodity.get("burger"))
+    })
+
+    console.log(burgerEvent)
+    console.log(user.hasCommodity.entries())
+
+    return burger;
 }
 
 function createUserInfo(user) {
-    const userInfo = `
+    const userInfo = document.createElement("div");
+    userInfo.classList.add("col-7");
+    userInfo.innerHTML = `
         <div id="userInfo" class="container bg-navy">
             <div class="row row-cols-1 row-cols-sm-2">
                 <div id="name" class="col">${user.name}</div>
-                <div id="old" class="col">${user.old}</div>
+                <div id="age" class="col">${user.age}</div>
                 <div id="days" class="col">${user.days}</div>
                 <div id="money" class="col">${user.money}</div>
             </div>
         </div>
+        <div>123456</div>
     `;
 
     return userInfo;
